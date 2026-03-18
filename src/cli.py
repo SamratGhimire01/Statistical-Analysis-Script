63ca25e01062d5d8263d94724bc6a2be7bfdf3ba
SamratGhimire01 <samratghimire01@gmail.com>
2026-03-18 21:48:29 +0545

    feat: add hte code of cli for input and ouput.

---
+++ src/cli.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+def get_args():
+    parse = ArgumentParser(description= "statistical-analysis")
+    parse.add_argument("--input",required=True, help="Getting the input of CSV file.")
+    parse.add_argument("--output",default="result.csv", help="Giving the output of CSV file after the processing.")
+    parse.add_argument("--format",choices=['console','json'],default="console", help="Showing the result.")
+    args = parse.parse_args()
+    return args
